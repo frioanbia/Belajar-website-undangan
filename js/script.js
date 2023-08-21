bbdaa54f2074cc58ecf0d4f4b51aa06de7341b4c
frio anbia <frioanbia63@gmail.com>
2023-08-21 16:40:28 +0700

    add rsvp

---
+++ js/script.js
@@ -0,0 +1,50 @@
+// nav bar
+const stickyTop = document.querySelector(".sticky-top");
+const myOffcanvas = document.querySelector(".offcanvas");
+const rootElement = document.querySelector(":root");
+
+myOffcanvas.addEventListener("show.bs.offcanvas", function () {
+  stickyTop.style.overflow = "visible";
+});
+
+myOffcanvas.addEventListener("hidden.bs.offcanvas", function () {
+  stickyTop.style.overflow = "hidden";
+});
+
+// fitur waktu mundur
+simplyCountdown(".simply-countdown", {
+  year: 2026, // required
+  month: 09, // required
+  day: 15, // required
+  hours: 8, // Default is 0 [0-23] integer
+  words: {
+    //words displayed into the countdown
+    days: { singular: "Hari", plural: "Hari" },
+    hours: { singular: "Jam", plural: "Jam" },
+    minutes: { singular: "Menit", plural: "Menit" },
+    seconds: { singular: "Detik", plural: "Detik" },
+  },
+});
+
+function disableScroll() {
+  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+
+  window.onscroll = function () {
+    window.scrollTo(scrollTop, scrollLeft);
+  };
+
+  rootElement.style.scrollBehavior = "auto";
+}
+
+function enableScroll() {
+  window.onscroll = function () {};
+  rootElement.style.scrollBehavior = "smooth";
+  localStorage.setItem("opened", "true");
+}
+
+if (!localStorage.getItem("opened")) {
+  disableScroll();
+}
+
+disableScroll();
