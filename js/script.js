81f6b453d5a720173c7ce752d0d7571e195e466a
frio anbia <frioanbia63@gmail.com>
2023-08-21 20:45:24 +0700

    add rsvp, gifts, and footer

--- js/script.js
+++ js/script.js
@@ -48,3 +48,18 @@ if (!localStorage.getItem("opened")) {
 }
 
 disableScroll();
+
+window.addEventListener("load", function () {
+  const form = document.getElementById("my-form");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: "POST",
+      body: data,
+    }).then(() => {
+      alert("Berhasil tersimpan, Terima kasih!");
+    });
+  });
+});
